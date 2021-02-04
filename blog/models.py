eb48f61e7763885712550c18678b8b22bf1f8494
limbo-envoy <1994051800@qq.com>
2021-02-04 18:34:02 +0800

    repair view count bug

--- blog/models.py
+++ blog/models.py
@@ -81,6 +81,8 @@ class Post(models.Model):
     # 因为我们规定一篇文章只能有一个作者，而一个作者可能会写多篇文章，因此这是一对多的关联关系，和
     # Category 类似。
     author = models.ForeignKey(User, verbose_name='作者', on_delete=models.CASCADE)
+    # 新增 views 字段记录阅读量
+    views = models.PositiveIntegerField(verbose_name='阅读量', default=0, editable=False)
 
     class Meta:
         verbose_name = '文章'
@@ -110,3 +112,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('blog:detail', kwargs={'pk': self.pk})
 
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
