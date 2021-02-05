e28e95e1e5e52694bd1e217e02379196355598c9
limbo-envoy <1994051800@qq.com>
2021-02-05 11:33:20 +0800

    change type views

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('posts/<int:pk>/', views.detail, name='detail'),
-    path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
-    path('categories/<int:pk>/', views.category, name='category'),
-    path('tags/<int:pk>/', views.tag, name='tag'),
+    # path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    # path('posts/<int:pk>/', views.detail, name='detail'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    # path('archives/<int:year>/<int:month>/', views.archive, name='archive'),
+    path('archives/<int:year>/<int:month>/', views.ArchiveViews.as_view(), name='archive'),
+    # path('categories/<int:pk>/', views.category, name='category'),
+    path('categories/<int:pk>/', views.CategoryView.as_view(), name='category'),
+    # path('tags/<int:pk>/', views.tag, name='tag'),
+    path('tags/<int:pk>/', views.TagView.as_view(), name='tag'),
 ]
